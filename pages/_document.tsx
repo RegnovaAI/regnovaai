8ce38496624f29949faf0c2622bb4051f9bb7537
founder@regnovaAI.com <founder@regnovaAI.com>
2025-05-07 21:45:43 +0200

    hi aaa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,27 @@
 // pages/_document.tsx
-import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <script
-          async
-          defer
-          data-domain="regnovaai-frontend-git-c8ee04-rajeev-ranjans-projects-572f1b56.vercel.app"
-          src="https://plausible.io/js/plausible.js"
-        ></script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* ✅ Plausible Analytics Script */}
+          <script
+            async
+            defer
+            data-domain="regnovaai-frontend-git-c8ee04-rajeev-ranjans-projects-572f1b56.vercel.app"
+            src="https://plausible.io/js/plausible.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
+export default MyDocument;
