f56f912ab4eb0dd4b923c4d8398c8f204888b960
founder@regnovaAI.com <founder@regnovaAI.com>
2025-05-07 20:08:49 +0200

    traffic

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* ✅ Plausible Analytics Script */}
+        <script
+          async
+          defer
+          data-domain="regnovaai.com"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
